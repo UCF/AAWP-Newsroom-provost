6c205e8ce9648680bcbde9f71caff814f58ead0d
Mark Bennett <mark.bennett@ucf.edu>
2020-02-11 16:39:59 -0500

    Added category and academic unit filters to the search bar

--- searchform.php
+++ searchform.php
@@ -1,5 +1,69 @@
-  <form role="search" method="get" action="<?php echo home_url( '/' ); ?>" class="input-group search-input">
-  	<input type="search" class="form-control" placeholder="Search the newsroom" value="<?php echo get_search_query() ?>" name="s" title="Search" />
-    <button type="submit" class="search-submit btn btn-primary" title="Search Button"><i class="fa fa-search" aria-hidden="true"></i> </button>
+  <form role="search" method="get" action="<?php echo home_url( '/' ); ?>" class="search-input">
+    <div class="form-group">
+  	   <input type="search" class="form-control" placeholder="Search the newsroom" value="<?php echo get_search_query() ?>" name="s" title="Search" />
+    </div>
+    <div class="form-group row">
+
+      <div class="col">
+
+        <?php
+
+
+        if ( get_query_var('cat') ) {
+            // If so echo the value
+            $cat = get_query_var('cat');
+        }
+
+        wp_dropdown_categories( array(
+  			//	'show_option_all' => 'All Categories',
+  				'show_option_none'  => 'All Categories',
+          'option_none_value'  => '',
+  				'orderby' => 'name',
+  				'echo' => 1,
+  				'selected' => $cat,
+  				'hierarchical' => true,
+          'name' => 'cat',
+  				'class'	=> 'cat-dropdown  form-control',
+  				'id'	=> 'custom-cat-drop',
+  				'value_field' => 'term_id'
+  			) ); ?>
+
+    </div>
+
+    <div class="col">
+
+      <?php
+
+
+      if ( get_query_var('units') ) {
+          // If so echo the value
+          $au = get_query_var('units');
+
+      }
+
+      wp_dropdown_categories( array(
+        'show_option_none'  => 'All Academic Units',
+        'option_none_value'  => '',
+        'orderby' => 'name',
+        'echo' => 1,
+        'selected' => $au,
+        'taxonomy' => 'academic_units',
+        'hierarchical' => true,
+        'name' => 'units',
+        'class'	=> 'unit-dropdown form-control',
+        'id'	=> 'academic-unit-drop',
+        'value_field' => 'slug'
+      ) ); ?>
+
+
+  </div>
+
+  <div class="col-12 col-md-2">
+
+    <button type="submit" class="search-submit btn btn-primary" title="Search Button">Search</button>
+
+  </div>
+
+  </div>
 
   </form>
