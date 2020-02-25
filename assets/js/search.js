bf6dc20edd10de8831876abbee5cf7b8f3eefe2f
Mark Bennett <mark.bennett@ucf.edu>
2020-02-25 10:29:29 -0500

    search js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,9 +1,13 @@
 $(function() {
 
-
-
   $( ".newsroom-search" ).on( "click", function() {
-    event.preventDefault();
+    var mobile = $( "#header-menu" ).hasClass( "show" );
+
+      if( mobile == true){
+      return;
+    }
+
+        event.preventDefault();
 
     $( ".search-bar" ).slideToggle("slow");
 
@@ -11,25 +15,19 @@ $(function() {
   });
 
 
-
-
-
-
-});
-
-jQuery(document).ready(function($){
-
   // Remove empty fields from GET forms
   // Author: Bill Erickson
   // URL: http://www.billerickson.net/code/hide-empty-fields-get-form/
 
-  	// Change 'form' to class or ID of your specific form
-	$(".search-input").submit(function() {
-		$(this).find(".cat-dropdown, .unit-dropdown").filter(function(){ return !this.value; }).attr("disabled", "disabled");
-		return true; // ensure form still submits
-	});
+    // Change 'form' to class or ID of your specific form
+  $(".search-input").submit(function() {
+    $(this).find(".cat-dropdown, .unit-dropdown").filter(function(){ return !this.value; }).attr("disabled", "disabled");
+    return true; // ensure form still submits
+  });
 
-	// Un-disable form fields when page loads, in case they click back after submission
-	$( "form" ).find( ".cat-dropdown, .unit-dropdown" ).prop( "disabled", false );
+  // Un-disable form fields when page loads, in case they click back after submission
+  $( "form" ).find( ".cat-dropdown, .unit-dropdown" ).prop( "disabled", false );
+
+
 
 });
