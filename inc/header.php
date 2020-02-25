5b7059470fe9645de1542a46f65344703679d833
Mark Bennett <mark.bennett@ucf.edu>
2020-02-25 10:28:35 -0500

    fixed mobile issues

--- inc/header.php
+++ inc/header.php
@@ -7,39 +7,43 @@ function athena_custom_logo_setup() {
  'flex-width'  => true,
  'header-text' => array( 'site-title', 'site-description' ),
  );
- add_theme_support( 'custom-logo', $defaults );
+ //add_theme_support( 'custom-logo', $defaults );
 }
 
 
 
 function ucfwp_get_header_markup(){?>
 
-  <nav class="navbar navbar-toggleable-md navbar-news-custom news-nav pt-md-0 pb-md-0" role="navigation" aria-label="Site navigation">
+  <?php $menu_container_class = 'collapse navbar-collapse'; ?>
+
+  <?php
+  $menu =  wp_nav_menu( array(
+    'container'       => 'div',
+    'container_class' => 'collapse navbar-collapse',
+    'container_id'    => 'header-menu',
+    'depth'           => 2,
+    'echo'            => false,
+    'fallback_cb'     => 'bs4Navwalker::fallback',
+    'menu_class'      => 'nav navbar-nav ml-md-auto',
+    'theme_location'  => 'header-menu',
+    'walker'          => new bs4Navwalker()
+  ) );
+  ?>
+
+  <nav class="navbar navbar-toggleable-md navbar-custom news-nav" role="navigation" aria-label="Site navigation">
     <div class="container d-flex flex-row flex-nowrap justify-content-between">
-      <span class="logo">
-        <?php/* if ( has_custom_logo() ) {
-          the_custom_logo();
-        } else { */?>
-        <a class="navbar-brand mr-lg-5 text-uppercase" href="<?php echo esc_url( home_url( '/' )); ?>"> Provost &nbsp; <span class="text-primary">Newsroom</span></a>
-        <?php  /* } */ ?>
+      <span class="mb-0">
+        <a class="navbar-brand mr-lg-5 text-uppercase" href="<?php echo esc_url( home_url( '/' )); ?>"> <?php echo bloginfo( 'name' ); ?></a>
+
       </span>
+      <?php if ( $menu ): ?>
       <button class="navbar-toggler ml-auto align-self-start collapsed" type="button" data-toggle="collapse" data-target="#header-menu" aria-controls="header-menu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-text">Navigation</span>
-        <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon "><i class="fa fa-bars fa-2 my-auto" aria-hidden="true"></i></span>
       </button>
+      <?php echo $menu; ?>
+      <?php endif; ?>
 
-      <?php
-      wp_nav_menu( array(
-        'container'       => 'div',
-        'container_class' => 'collapse navbar-collapse align-self-lg-stretch',
-        'container_id'    => 'header-menu',
-        'depth'           => 2,
-        'fallback_cb'     => 'bs4Navwalker::fallback',
-        'menu_class'      => 'nav navbar-nav ml-md-auto',
-        'theme_location'  => 'header-menu',
-        'walker'          => new bs4Navwalker()
-      ) );
-      ?>
     </div>
   </nav>
 
