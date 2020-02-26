46c7b3f8ac5f84c99f4fabd3eab411c0bebf8ac6
Mark Bennett <mark.bennett@ucf.edu>
2020-02-26 16:06:10 -0500

    template update
    Mobile friendly

--- archive.php
+++ archive.php
@@ -6,19 +6,19 @@
     <article class="term-list-item mb-4 py-5 divider">
       <?php $link = get_permalink(); ?>
       <div class="row">
-        <div class="order-1 order-sm-0 col-12 col-sm-6 col-md-8">
-            <p class="font-italic entry-date"><?php echo esc_html( get_the_date('D M j')); ?></p>
-    	       <h2 class="h3 archive-title mb-5 text-secondary"><a href="<?php echo $link; ?>" class="text-secondary"><?php the_title(); ?> </a></h2>
-             <div class="read-more text-uppercase font-weight-bold"><a href="<?php echo $link; ?>" class="btn btn-outline-secondary btn-sm">Read More ></a></div>
-       </div>
-        <div class="order-0 order-sm-1 col-12 col-sm-6 col-md-4">
+        <div class="flex-md-last col-12 col-md-4">
             <?php if(has_post_thumbnail()): ?>
-                    <div class="archive-thumbnail"> <a href="<?php echo $link; ?>"> <?php the_post_thumbnail( 'archive_thumb' ); ?> </a><div>
+                    <div class="archive-thumbnail pb-4"> <a href="<?php echo $link; ?>"> <?php the_post_thumbnail( 'archive_thumb' ); ?> </a></div>
             <?php else: ?>
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/provost-newsroom.jpg"  width="200" height="200" alt="Provost news deafult image"/>
             <?php endif; ?>
 
         </div>
+        <div class=" order-sm-1 col-12 col-md-8">
+            <h2 class="h3 archive-title mb-2 text-secondary"><a href="<?php echo $link; ?>" class="text-secondary"><?php the_title(); ?> </a></h2>
+            <p class="font-italic entry-date"><?php echo esc_html( get_the_date('D M j')); ?></p>
+             <div class="read-more text-uppercase font-weight-bold"><a href="<?php echo $link; ?>" class="btn btn-outline-secondary btn-sm">Read More ></a></div>
+       </div>
     	</div>
     </article>
 
