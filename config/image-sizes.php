76e88ac5ea8dade979f65a926e24e76ba5cef0dd
Mark Bennett <mark.bennett@ucf.edu>
2020-03-10 10:18:01 -0400

    removed ucf image sizes

--- config/image-sizes.php
+++ config/image-sizes.php
@@ -1,4 +1,26 @@
 <?php
+
+
+/*
+remove UCF wordpres theme default image sizes
+*/
+function remove_theme_image_sizes() {
+ // Remove page header image sizes, since the UCF WP Theme's
+ // media header logic isn't utilized in this theme.
+ remove_image_size( 'header-img' );
+ remove_image_size( 'header-img-sm' );
+ remove_image_size( 'header-img-md' );
+ remove_image_size( 'header-img-lg' );
+ remove_image_size( 'header-img-xl' );
+ remove_image_size( 'bg-img' );
+ remove_image_size( 'bg-img-sm' );
+ remove_image_size( 'bg-img-md' );
+ remove_image_size( 'bg-img-lg' );
+ remove_image_size( 'bg-img-xl' );
+}
+
+add_action( 'after_setup_theme', 'remove_theme_image_sizes', 11 );
+
 /*
 Custom image sizes
  */
