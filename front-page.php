d8542eb5c938c56127770833e0174a87f0a72e2e
TheMarkBennett <mark.bennett@ucf.edu>
2022-03-21 13:54:56 -0400

    Updating Homepage

--- front-page.php
+++ front-page.php
@@ -1,24 +1,17 @@
 <?php get_header(); the_post(); ?>
+	<div class="main mb-5 pb-sm-4"> 
+		<div class="row">
+			<div class="col-12 col-md-7">
+				
+			</div>
+			<div class="col-12 col-md-5">
+				
+			</div>
+		</div>
+	</div>
 
 
-	<div class="main mb-5 pb-sm-4">
-		<?php  provost_news_alerts()  ?>
-    <?php provost_news_featured_article(); ?>
-			
-				<section class="provost-section">
-        <?php provost_news_section() ?>
-			</section>
-				<section class="pn-tax">
-				<?php provost_news_featured_tax(); ?>
-			</section>
-			<?php if ( is_active_sidebar( 'front-sidebar' ) ) : ?>
-				<section class="ucf-today pt-4">
-					<div class="container">
-					<?php dynamic_sidebar( 'front-sidebar' ); ?>
-					</div>
-				</section>
-				<?php endif; ?>
-      <?php //get_sidebar(); ?>
+
 
 
 
