a032ab8a2d8674f59b45fa208b8f9f6bd543e876
TheMarkBennett <mark.bennett@ucf.edu>
2022-12-09 09:03:35 -0500

    exclude nav

--- header.php
+++ header.php
@@ -14,8 +14,7 @@
         $obj         = ucfwp_get_queried_object();
         $exclude_nav = get_field( 'page_header_exclude_nav', $obj );
         ?>
-        <?php if ( ! $exclude_nav ) { echo ucfwp_get_nav_markup( false ); } ?>
-        <?php echo ucfwp_get_header_markup(); ?>
+        <?php if ( ! $exclude_nav ) { echo ucfwp_get_header_markup(); } ?>
     </header>
     <main class="site-main">
         <?php echo ucfwp_get_subnav_markup(); ?>
