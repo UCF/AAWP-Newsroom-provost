947f8d9dcac651d9a68bc3b274ccd5c1dfecc482
TheMarkBennett <mark.bennett@ucf.edu>
2022-12-09 08:59:28 -0500

    get acf field exclude

--- header.php
+++ header.php
@@ -10,6 +10,10 @@
     <a class="skip-navigation bg-complementary text-inverse box-shadow-soft" href="#content">Skip to main content</a>
     <div id="ucfhb"></div>
     <header class="site-header">
+        <?php
+        $obj         = ucfwp_get_queried_object();
+        $exclude_nav = get_field( 'page_header_exclude_nav', $obj );
+        ?>
         <?php if ( ! $exclude_nav ) { echo ucfwp_get_nav_markup( false ); } ?>
         <?php echo ucfwp_get_header_markup(); ?>
     </header>
